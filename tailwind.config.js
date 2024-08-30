281587d1e4f631f873bdc4d4792592756531cd2f
MohammadSoleimanikia <121093560+MohammadSoleimanikia@users.noreply.github.com>
2024-08-30 17:40:57 +0330

    add hero section mobile mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
   content: ["./docs/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily:{
+        inter:['Inter']
+      },
+      boxShadow:{
+        '3xl':"1rem 2rem 3.5rem rgba(143, 174, 207, 0.25)"
+    }
+    },
   },
   plugins: [],
 }
